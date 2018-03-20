2797c06e95bb9e888176f134f463e0a30886d263
Jim Bevington <jim.bevington@hotmail.com>
2018-03-20 16:26:06 +0000

    added engineers view all route, engineers/index.vtl template and engineers create route

--- src/main/java/controllers/EngineersController.java
+++ src/main/java/controllers/EngineersController.java
@@ -1,4 +1,58 @@
 package controllers;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import com.sun.tools.internal.xjc.model.Model;
+import db.DBHelper;
+import models.Department;
+import models.Engineer;
+import org.dom4j.rule.Mode;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static spark.Spark.get;
+
 public class EngineersController {
+
+    public EngineersController() {
+        this.setupEndpoints();
+    }
+
+    private void setupEndpoints() {
+
+//        view all
+        get("/engineers", (req, res) -> {
+
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("template", "templates/engineers/index.vtl");
+
+            List<Engineer> engineers = DBHelper.getAll(Engineer.class);
+            model.put("engineers", engineers);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+
+        }, new VelocityTemplateEngine());
+
+//        create new
+        get("/engineers/new", (req, res) -> {
+
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("template", "templates/engineers/create.vtl");
+
+            List<Department> departments = DBHelper.getAll(Department.class);
+            model.put("departments", departments);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+
+        }, new VelocityTemplateEngine());
+
+
+//        save new
+
+
+    }
+
+
 }
